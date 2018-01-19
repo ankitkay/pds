2cb40d31e73f0859f881ab1305bf136fadf45b94
ankkay <ankitkay95@gmail.com>
2018-01-20 01:06:03 +0530

    having error to find resource , requested resource not available

---
+++ pdsfront/pds/src/main/java/dac/pds/controller/PageController.java
@@ -0,0 +1,16 @@
+package dac.pds.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	@RequestMapping(value={"/","/home","/index","/pds"})
+	public ModelAndView index(){
+	ModelAndView mv = new ModelAndView("page");
+	mv.addObject("greeting","welcome");
+	return mv;
+	
+	}
+}
